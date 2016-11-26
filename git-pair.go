f20fa9a84d50235bd906aeb3fbe422f382f5c715
Ryan Wood <ryanstatenwood@gmail.com>
2016-11-26 07:44:08 -0800

    Make go fmt happy

--- git-pair.go
+++ git-pair.go
@@ -5,30 +5,30 @@ import "encoding/json"
 import "fmt"
 
 type GithubUser struct {
-  Name string `json:"name"`
-  Email string `json:"email"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
 }
 
 func main() {
 	resp, err := http.Get("https://api.github.com/users/ryanswood")
 
-  if err != nil {
-    panic(err.Error())
-  }
+	if err != nil {
+		panic(err.Error())
+	}
 
 	defer resp.Body.Close()
 
-  ghu := new(GithubUser)
+	ghu := new(GithubUser)
 
-  json.NewDecoder(resp.Body).Decode(&ghu)
+	json.NewDecoder(resp.Body).Decode(&ghu)
 
-  fmt.Println(ghu)
+	fmt.Println(ghu)
 }
 
-  // body, err := ioutil.ReadAll(resp.Body)
+// body, err := ioutil.ReadAll(resp.Body)
 
-  // if err != nil {
-  //   panic(err.Error())
-  // }
+// if err != nil {
+//   panic(err.Error())
+// }
 
-  // fmt.Print(body)
+// fmt.Print(body)
