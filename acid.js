c52293bc4f2dbc1fd313578896d6d00a8d0d9ff4
Sean Knox <knoxville@gmail.com>
2017-04-20 11:54:49 -0700

    add acid.js

---
+++ acid.js
@@ -0,0 +1,20 @@
+// Acid CI/CD
+
+// We need to simulate a Go environment
+gopath = "/go";
+localPath = gopath + "/src/github.com/" + pushRecord.repository.full_name;
+
+// Define a single build step:
+j = new Job("unit-tests");
+j.image = "golang:1.7.5";
+j.env = {
+  "DEST_PATH": localPath,
+  "GOPATH": gopath
+};
+
+j.tasks = [
+  "make bootstrap",
+  "make test"
+];
+
+j.run(pushRecord).waitUntilDone()
